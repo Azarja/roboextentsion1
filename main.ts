ffd9564d25890d0148bd989cfa5ac6dcd0eb1dec
Azarja <a.nieman@optec.nl>
2021-03-19 16:10:40 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     loop1 += 1
-    basic.showNumber(0)
+    basic.showNumber(loop1)
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 113)
+    basic.showString("motor started")
+})
+input.onButtonPressed(Button.B, function () {
+    motor.motorStop(motor.Motors.M1)
+    basic.showString("motor stopped")
 })
 let loop1 = 0
+loop1 = 0
 basic.forever(function () {
 	
 })
