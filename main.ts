b6801b972f07a150bea6f67d98f1d3f96d340c1d
Azarja <a.nieman@optec.nl>
2021-03-19 16:16:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 input.onButtonPressed(Button.A, function () {
-    loop1 += 1
-    basic.showNumber(loop1)
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 113)
-    basic.showString("motor started")
+    basic.showLeds(`
+        # # # . .
+        . # # # .
+        . . # # #
+        . # # # .
+        # # # . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     motor.motorStop(motor.Motors.M1)
-    basic.showString("motor stopped")
+    basic.showIcon(IconNames.No)
 })
 let loop1 = 0
-loop1 = 0
 basic.forever(function () {
 	
 })
