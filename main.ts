7ab1b55de216dd56ab8c7ee8402834007775f062
Azarja <a.nieman@optec.nl>
2021-03-19 18:07:26 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
-input.onPinPressed(TouchPin.P0, function () {
+input.onButtonPressed(Button.A, function () {
+    if (loop1 == 0) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 113)
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # #
+            . # # # .
+            # # # . .
+            `)
+        loop1 = 1
+    } else {
+        motor.motorStop(motor.Motors.M1)
+        basic.showIcon(IconNames.No)
+        loop1 = 0
+    }
+})
+input.onButtonPressed(Button.B, function () {
     motor.servo(motor.Servos.S1, 0)
     basic.showIcon(IconNames.SmallSquare)
     motor.servo(motor.Servos.S1, 180)
 })
-input.onButtonPressed(Button.A, function () {
-    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 113)
-    basic.showLeds(`
-        # # # . .
-        . # # # .
-        . . # # #
-        . # # # .
-        # # # . .
-        `)
-})
-input.onButtonPressed(Button.B, function () {
-    motor.motorStop(motor.Motors.M1)
-    basic.showIcon(IconNames.No)
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Happy)
 })
 let loop1 = 0
-basic.forever(function () {
-	
-})
+loop1 = 0
