2ad1c0ff3fff2f771966c22cfa4daba459a5af21
Azarja <a.nieman@optec.nl>
2021-03-21 08:58:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,9 +15,34 @@ input.onButtonPressed(Button.A, function () {
         loop1 = 0
     }
 })
-input.onPinPressed(TouchPin.P1, function () {
-    basic.showIcon(IconNames.Happy)
-    basic.pause(500)
+input.onButtonPressed(Button.B, function () {
+    if (loop1 == 0) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 113)
+        basic.showLeds(`
+            # # # . .
+            . # # # #
+            . . # # #
+            . # # # .
+            # # # . .
+            `)
+        loop1 = 2
+    } else if (loop1 == 1) {
+        motor.motorStop(motor.Motors.M1)
+        basic.pause(500)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 113)
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # #
+            . # # # .
+            # # # . .
+            `)
+        loop1 = 2
+    } else {
+        motor.motorStop(motor.Motors.M1)
+        basic.showIcon(IconNames.No)
+        loop1 = 0
+    }
 })
 let loop1 = 0
 loop1 = 0
